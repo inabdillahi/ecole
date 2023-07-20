7bf58fb6fae6079414b520aee161f0c35b69c9aa
inabdillahi <formation.ete.2020@gmail.com>
2023-07-20 12:45:55 +0300

    Mise en place du theme

---
+++ src/Controller/TableauController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TableauController extends AbstractController
+{
+    #[Route('/', name: 'tableau')]
+    public function tableau(): Response
+    {
+        return $this->render('tableau/tableau.html.twig');
+    }
+}
